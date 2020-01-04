2d6f780b76f5f6115565424e4c7bd4d3eee631ec
ren.meta@gmail.com <ren.meta@gmail.com>
2020-01-04 16:14:29 -0500

    Deployed to heroku, tested endpoints in insomnia-- working!

--- djorg/settings.py
+++ djorg/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]' ,'.herokuapp.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 # ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Application definition
